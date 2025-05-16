976024814b533c2c72cd2bb2ef38bb955aa88fd1
FacuJesus <facundojesus10@hotmail.com>
2025-05-16 18:22:49 -0300

    Positivo, negativo o cero

---
+++ Guía 7.2/Ejercicio 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+
+            Console.WriteLine("Ingrese el número a evaluar:");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            if (num < 0) {
+                Console.WriteLine("El número es NEGATIVO");
+            }
+            else if (num == 0){
+                Console.WriteLine("El número es CERO");
+            }
+            else {
+                Console.WriteLine("El número es POSITIVO");
+            }
+
+
+        }
+    }
+}
