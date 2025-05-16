e9995901738ca31911db03b8ed0ab52ee56bd85d
FacuJesus <facundojesus10@hotmail.com>
2025-05-16 18:22:18 -0300

    Condición del alumno

---
+++ Guía 7.2/Ejercicio 1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+            double n1, n2, n3, condicion, promedio;
+
+            Console.WriteLine("Ingrese la condición de promoción:");
+            condicion = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la 1º nota:");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la 2º nota:");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la 3º nota:");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            promedio = (n1 + n2 + n3) / 3;
+
+            if (promedio > condicion){
+                Console.WriteLine("El alumno promociona");
+            }
+            else {
+                Console.WriteLine("El alumno Rinde final");
+            }
+        }
+    }
+}
